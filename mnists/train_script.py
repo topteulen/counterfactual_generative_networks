7afa90d38df84d28949bfc915d594d89d544b091
LVeefkind <larsveefkind@gmail.com>
2022-05-24 13:55:27 +0200

    added partial results

--- mnists/train_script.py
+++ mnists/train_script.py
@@ -8,9 +8,11 @@ datasets = ["colored_MNIST", "double_colored_MNIST", "wildlife_MNIST"]
 additions = ['', '_counterfactual', '_counterfactual_rot', '_counterfactual_rot_scale', '_counterfactual_rot_scale_shear']
 
 
-for model in models:
+for model in models[3:]:
     for dataset in datasets:
         for addition in additions:
+            if dataset == "colored_MNIST" and "scale" not in addition and model == "SO2SteerableCNN":
+                continue
             cmd = f'python mnists/train_classifier.py --dataset {dataset}{addition} --model {model}'
             print(f"\nRunning:\n {cmd}\n")
             result = subprocess.check_output(cmd, shell=True, text=True)
